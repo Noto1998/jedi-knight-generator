acb859618d10b5ac0986ff6683d0dbfa06bddfe4
Noto <1358519169@qq.com>
2020-12-10 15:12:17 +0800

    简易实现

---
+++ js/generator.js
@@ -0,0 +1,22 @@
+let generator = value => `首先，你要知道${value}是干什么的。其次，要知道成为${value}的资格是什么。` +
+    `第三，找出自己与前两项不一样，或者有差距的地方。第四，根据差距完善自己。第五，成为${value}。`
+
+let changeText = value => {
+    document.getElementById("results").innerHTML = generator(value)
+}
+
+window.onload = () => {
+    
+    changeText("绝地武士")// init
+
+    document.getElementById("btn").addEventListener("click", () => {
+        let value = document.getElementById("input").value
+        if ("" != value) {
+            changeText(value)
+        }
+    })
+
+    document.getElementById("copy").addEventListener("click", () => {
+        navigator.clipboard.writeText(document.getElementById("results").innerHTML)
+    })
+}
